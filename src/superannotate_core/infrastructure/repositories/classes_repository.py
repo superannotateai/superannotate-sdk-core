6b5d4f784e94d2e95bd1563fa9f607e98d4f1a7d
Narek Mkhitaryan <narekm@Nareks-MacBook-Pro.local>
2024-07-16 17:30:14 +0400

    updated classes interface

--- src/superannotate_core/infrastructure/repositories/classes_repository.py
+++ src/superannotate_core/infrastructure/repositories/classes_repository.py
@@ -3,13 +3,15 @@ from typing import List
 
 from superannotate_core.core.conditions import Condition
 from superannotate_core.core.entities import AnnotationClassEntity
-from superannotate_core.infrastructure.repositories.base import BaseHttpRepositry
+from superannotate_core.core.utils import chunkify
+from superannotate_core.infrastructure.repositories.base import BaseHttpRepository
 
 
-class AnnotationClassesRepository(BaseHttpRepositry):
+class AnnotationClassesRepository(BaseHttpRepository):
     ENTITY = AnnotationClassEntity
     URL_LIST = "classes"
     URL_GET = "class/{}"
+    CHUNK_SIZE = 500
 
     def bulk_create(
         self, project_id: int, classes: List[AnnotationClassEntity]
@@ -17,14 +19,19 @@ class AnnotationClassesRepository(BaseHttpRepositry):
         params = {
             "project_id": project_id,
         }
-        response = self._session.request(
-            self.URL_LIST,
-            "post",
-            params=params,
-            json={"classes": [i.to_json(exclude_none=True) for i in classes]},
-        )
-        response.raise_for_status()
-        return self.serialize_entiy(response.json())
+        chunked_classes = chunkify(classes, self.CHUNK_SIZE)
+
+        created_classes: List[AnnotationClassEntity] = []
+        for chunk in chunked_classes:
+            response = self._session.request(
+                self.URL_LIST,
+                "post",
+                params=params,
+                json={"classes": [i.to_json(exclude_none=True) for i in chunk]},
+            )
+            response.raise_for_status()
+            created_classes.extend(self.serialize_entity(response.json()))
+        return created_classes
 
     def list(
         self, project_id: int, condition: Condition = None
@@ -38,14 +45,15 @@ class AnnotationClassesRepository(BaseHttpRepositry):
             else self.URL_LIST,
             query_params=params,
         )
-        return [self.serialize_entiy(i) for i in classes]
+        return self.serialize_entity(classes)
 
     def delete(self, project_id: int, annotation_class_id: int):
-        return self._session.request(
+        response = self._session.request(
             self.URL_GET.format(annotation_class_id),
             "delete",
             params={"project_id": project_id},
         )
+        return self.serialize_entity(response.json())
 
     def download(self, project_id: int, path: str, condition: Condition = None):
         classes = [entity.dict() for entity in self.list(project_id, condition)]
